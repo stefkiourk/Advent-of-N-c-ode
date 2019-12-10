f25e97be3f3e2ab2427c9290bbe3bbd4577f83ee
Stef Ki <stefanoskiourkoulis@gmail.com>
2019-12-11 00:08:25 +0100

    Solution for Day 1

---
+++ day1.js
@@ -0,0 +1,7 @@
+require("fs").readFile('./input.txt', 'utf8', (err, data) => {
+    console.log(
+        data.split("\n")
+        .map(x => Math.floor(x/3) - 2)
+        .reduce((a, b) => a +b)
+    );
+});
